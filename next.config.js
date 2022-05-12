0ef4802fd8577f9d7633ef808a6add25eadee61b
Yasa Mulky Al Afgani <mulkyasa@gmail.com>
2022-05-12 07:40:35 +0700

    update: configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com', 'res.cloudinary.com'],
+  },
+  webpack(config, _options) {
+    config.resolve.alias['@pages'] = path.join(__dirname, 'pages');
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@containers'] = path.join(__dirname, 'containers');
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
