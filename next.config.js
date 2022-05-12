669b41f73e2b81ab43dbf4e63313a6abf20818f8
Yasa Mulky Al Afgani <mulkyasa@gmail.com>
2022-05-12 08:07:16 +0700

    update: configs

--- next.config.js
+++ next.config.js
@@ -7,8 +7,10 @@ const nextConfig = {
   },
   webpack(config, _options) {
     config.resolve.alias['@pages'] = path.join(__dirname, 'pages');
-    config.resolve.alias['@components'] = path.join(__dirname, 'components');
     config.resolve.alias['@containers'] = path.join(__dirname, 'containers');
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+    config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
+    config.resolve.alias['@models'] = path.join(__dirname, 'models');
 
     return config;
   },
